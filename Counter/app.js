7aee23571d3bf577a6a4ee6bad559f678e61289a
Beiko14 <heiko.hollerith14@gmx.de>
2021-06-21 13:14:05 +0200

    created a simple Counter

---
+++ Counter/app.js
@@ -0,0 +1,30 @@
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+// console.log(btns);
+
+btns.forEach(btn => {
+    console.log(btn);
+    btn.addEventListener('click', function(e){
+
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        } else if(styles.contains('reset')){
+            count = 0;
+        } else if(styles.contains('increase')){
+            count++;
+        }
+
+        if(count > 0){
+            value.style.color = "green";
+        } else if(count < 0){
+            value.style.color = "red";
+        }
+
+        value.textContent = count;
+
+    })
+});
